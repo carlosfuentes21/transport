37949a55a6b2cc0f40fa7438f887349eb4ce5060
carlos.fuentes <carlos.fuentes@pragma.com.co>
2024-07-18 19:52:45 -0500

    add: timed trigger infraestructure

---
+++ infrastructure/entry-points/cron-scheduling/src/main/java/co/com/sofka/cronscheduling/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package co.com.sofka.cronscheduling.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
