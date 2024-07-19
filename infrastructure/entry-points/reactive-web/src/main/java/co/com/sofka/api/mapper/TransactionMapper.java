1d99c929b8cb5945fca12e81c2e0c39861c27898
carlos.fuentes <carlos.fuentes@pragma.com.co>
2024-07-19 09:36:46 -0500

    fix: validate format date and exclude jacoco

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/mapper/TransactionMapper.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/sofka/api/mapper/TransactionMapper.java
@@ -4,13 +4,16 @@ import co.com.sofka.api.dto.TransactionDto;
 import co.com.sofka.model.Transaction;
 import lombok.experimental.UtilityClass;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @UtilityClass
 public class TransactionMapper {
 
     public static Transaction toDomain(TransactionDto transactionDto) {
         return Transaction.builder()
                 .transactionId(transactionDto.getTransactionId())
-                .timestamp(transactionDto.getTimestamp())
+                .timestamp(convertStringToLocalDateTime(transactionDto.getTimestamp()))
                 .deviceNumber(transactionDto.getDeviceNumber())
                 .userId(transactionDto.getUserId())
                 .geoPosition(transactionDto.getGeoPosition())
@@ -21,12 +24,22 @@ public class TransactionMapper {
     public static TransactionDto toDto(Transaction transaction) {
         return TransactionDto.builder()
                 .transactionId(transaction.getTransactionId())
-                .timestamp(transaction.getTimestamp())
+                .timestamp(convertLocalDateTimeToString(transaction.getTimestamp()))
                 .deviceNumber(transaction.getDeviceNumber())
                 .userId(transaction.getUserId())
                 .geoPosition(transaction.getGeoPosition())
                 .amount(transaction.getAmount())
                 .build();
+    }
+
+    public static LocalDateTime convertStringToLocalDateTime(String timestamp) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        return LocalDateTime.parse(timestamp, formatter);
+    }
+
+    public static String convertLocalDateTimeToString(LocalDateTime localDateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        return localDateTime.format(formatter);
     }
 
 }
